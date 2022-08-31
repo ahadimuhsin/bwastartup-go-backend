6f8fb50534dbd812fd560fdf96c8433d9c46f9bf
Muhsin Ahadi <muhsin_ahadi@student.uns.ac.id>
2022-08-31 15:00:31 +0700

    struct campaign and campaign images

---
+++ campaign/model.go
@@ -0,0 +1,29 @@
+package campaign
+
+import "time"
+
+//entity = model
+
+type Campaign struct{
+	ID 					int
+	UserId				int
+	Name 				string
+	ShortDescription	string
+	Description			string
+	Perks				string
+	BackerCount			int
+	GoalAmount			int
+	CurrentAmount		int
+	Slug				string
+	CreatedAt 			time.Time
+	UpdatedAt 			time.Time
+}
+
+type CampaignImage struct{
+	ID		int
+	CampaignId	int
+	FileName	string
+	IsPrimary	int
+	CreatedAt 			time.Time
+	UpdatedAt 			time.Time
+}
